a9b5ee652ad3bd29948e117651566202167ba6bd
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-20 19:50:52 +0900

    Created the connection variable to connect to MySql

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Driver {
     public static void main(String[] args) {
-
+        try{
+//            Get connection to database
+            Connection myConn = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/java_projects","admin","2727Gonzalo!"
+            );
+//            Create a statement
+//            Execute sql query
+//            process the result set
+        }
+        catch(Exception exc){
+            exc.printStackTrace();
+        }
     }
 }

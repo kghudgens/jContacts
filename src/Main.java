4e93ecf5adbf349a76213bdff7cd42eeef04b973
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-21 10:40:33 +0900

    Created welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+//        Create scanner object for repeated use
+        Scanner input = new Scanner(System.in);
+
+//        Welcome the user
+        System.out.println("Welcome to jContacts, a contact book powered by Java!");
+        System.out.println("What is your name?");
+
+        String userName = input.nextLine();
+        System.out.println("Hello " + userName + ", let us get started." );
+
+
 
     }
 }

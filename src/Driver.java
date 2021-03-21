464f3c755384ae230e658910912ebfc94c93d388
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-21 10:06:22 +0900

    Successfully connected to the database

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Driver {
     public static void main(String[] args) {
@@ -9,8 +11,13 @@ public class Driver {
                     "jdbc:mysql://127.0.0.1:3306/java_projects","admin","2727Gonzalo!"
             );
 //            Create a statement
+            Statement myStmt  = myConn.createStatement();
 //            Execute sql query
+            ResultSet myRs = myStmt.executeQuery("SELECT * from contacts");
 //            process the result set
+            while (myRs.next()){
+                System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+            }
         }
         catch(Exception exc){
             exc.printStackTrace();

8dbd3bce0febb8dced57a70c7e615f48084a98e9
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-23 12:27:54 +0900

    Created the viewSpecific function with BUG

--- src/Driver.java
+++ src/Driver.java
@@ -16,9 +16,31 @@ public class Driver {
             // process the result set into readable
             while (myRs.next()) {
                 System.out.println("Date added: " + myRs.getDate("submission_date") + "Last Name: "
-                        + myRs.getString("last_name") + ", First Name" + myRs.getString("first_name") + "Phone Number: "
-                        + myRs.getString("phone_number") + ", Email Address: " + myRs.getString("email")
-                        + "Occupation: " + myRs.getString("occupation"));
+                        + myRs.getString("last_name") + ", First Name: " + myRs.getString("first_name")
+                        + " Phone Number: " + myRs.getString("phone_number") + ", Email Address: "
+                        + myRs.getString("email") + " Occupation: " + myRs.getString("occupation"));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public static void viewSpecific(String last_name) {
+        try {
+            // Get connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java_projects", "admin",
+                    "2727Gonzalo!");
+            // Create a statement
+            Statement myStmt = myConn.createStatement();
+            // Execute sql query
+            // BUG fix the statment asking for last name
+            ResultSet myRs = myStmt.executeQuery("SELECT * from contacts WHERE last_name = " + last_name);
+            // process the result set into readable
+            while (myRs.next()) {
+                System.out.println("Date added: " + myRs.getDate("submission_date") + "Last Name: "
+                        + myRs.getString("last_name") + ", First Name: " + myRs.getString("first_name")
+                        + " Phone Number: " + myRs.getString("phone_number") + ", Email Address: "
+                        + myRs.getString("email") + " Occupation: " + myRs.getString("occupation"));
             }
         } catch (Exception exc) {
             exc.printStackTrace();

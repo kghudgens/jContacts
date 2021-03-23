152ee18a7b32765bc19fc7fcff78d11a16534f59
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-23 12:04:09 +0900

    imported the driver class, made the viewALL method

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        Create scanner object for repeated use
-        Scanner input = new Scanner(System.in);
+        static Driver driver = new Driver();
 
-//        Welcome the user
-        System.out.println("Welcome to jContacts, a contact book powered by Java!");
-        System.out.println("What is your name?");
+        public static void main(String[] args) {
+                // Create scanner object for repeated use
+                Scanner input = new Scanner(System.in);
 
-        String userName = input.nextLine();
-        System.out.println("Hello " + userName + ", let us get started." );
+                // Welcome the user
+                System.out.println("Welcome to jContacts, a contact book powered by Java!");
+                System.out.println("What is your name?");
 
-//        Create logic that decides what the user wants to do
-        System.out.println(userName + ", What you like to do?");
-        System.out.println("Your options are ADD a contact, VIEW a contact, and DELETE a contact");
-//        Get the users input
-        String userDecision = input.nextLine();
+                String userName = input.nextLine();
+                System.out.println("Hello " + userName + ", let us get started.");
 
-//       Enter if else statement to divide up whats next from the userDecision variable
-        if(userDecision.equals("ADD")){
-            System.out.println("You Selected ADD");
+                // Create logic that decides what the user wants to do
+                System.out.println(userName + ", What you like to do?");
+                System.out.println("Your options are ADD a contact, VIEW a contact, and DELETE a contact");
+                // Get the users input
+                String userDecision = input.nextLine();
+
+                // Enter if else statement to divide up whats next from the userDecision
+                // variable
+                // View first
+                if (userDecision.equals("VIEW")) {
+                        System.out.println("You Selected View");
+                        // Ask user if they want to see all contacts or specfic
+                        System.out.println("Would you like to see ALL contacts or a SPECIFIC contact");
+                        String viewAllorSpecific = input.nextLine();
+                        // Take the viewAllorSpecific and make if message with it
+                        if (viewAllorSpecific.equals("ALL")) {
+                                driver.viewAll();
+                        } else {
+
+                        }
+
+                }
+
         }
-
-
-
-    }
 }

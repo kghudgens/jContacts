2037e72bd435f426ea85898d67ba466a7910581a
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-23 11:48:37 +0900

    created view all function

--- src/Driver.java
+++ src/Driver.java
@@ -4,23 +4,25 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Driver {
-    public static void main(String[] args) {
-        try{
-//            Get connection to database
-            Connection myConn = DriverManager.getConnection(
-                    "jdbc:mysql://127.0.0.1:3306/java_projects","admin","2727Gonzalo!"
-            );
-//            Create a statement
-            Statement myStmt  = myConn.createStatement();
-//            Execute sql query
+    public static void viewAll() {
+        try {
+            // Get connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java_projects", "admin",
+                    "2727Gonzalo!");
+            // Create a statement
+            Statement myStmt = myConn.createStatement();
+            // Execute sql query
             ResultSet myRs = myStmt.executeQuery("SELECT * from contacts");
-//            process the result set
-            while (myRs.next()){
+            // process the result set
+            while (myRs.next()) {
                 System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
             }
-        }
-        catch(Exception exc){
+        } catch (Exception exc) {
             exc.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }

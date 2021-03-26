29cb97530f16e826aab31ec765129bd2734cb64f
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-26 13:41:31 +0900

    created the addContact Fucntion

--- src/Driver.java
+++ src/Driver.java
@@ -51,6 +51,30 @@ public class Driver {
         }
     }
 
+    public static void addContact(String first_name, String last_name, String phone_number, String email,
+            String occupation) {
+        try {
+            // Get connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java_projects", "admin",
+                    "2727Gonzalo!");
+            // Create a statement
+            Statement myStmt = myConn.createStatement();
+            // Execute sql query
+            ResultSet myRs = myStmt.executeQuery(
+                    "INSERT INTO contacts(first_name, last_name, phone_number, email, occupation) VALUES (" + "'"
+                            + first_name + "', '" + last_name + "'" + email + "', '" + occupation + "'");
+            // process the result set into readable
+            while (myRs.next()) {
+                System.out.println("Date added: " + myRs.getDate("submission_date") + ", Last Name: "
+                        + myRs.getString("last_name") + ", First Name: " + myRs.getString("first_name")
+                        + " Phone Number: " + myRs.getString("phone_number") + ", Email Address: "
+                        + myRs.getString("email") + " Occupation: " + myRs.getString("occupation"));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
     }

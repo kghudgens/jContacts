0b74415414f59f9cfb33fc1570a89a8546f633bd
Kevin Hudgens <hudgens1073@gmail.com>
2021-09-22 21:15:46 +0900

    use boolean variable to break loop

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class ContactBook {
 
         public static void main(String[] args) {
+                // To break out of the while loop containing the swith statement
+                boolean flag = true;
+
                 // Declare the userDecision variable in the global scope, so it can be used in
                 // the while loop
                 int userDecision;
+
                 // Create scanner object for repeated use
                 Scanner input = new Scanner(System.in);
                 System.out.println("\nWelcome to jContacts, a contact book powered by Java!");
+
                 // Create instance of the User class to interact with the database
                 User user = new User();
                 System.out.println("What would you like to do with your contacts?");
 
-                while (true) {
+                while (flag) {
                         System.out.println("1. Add\n2. View\n3. Update\n4. Delete\n5. Quit");
                         // Get the users input
                         userDecision = input.nextInt();
@@ -36,6 +41,8 @@ public class ContactBook {
                                 case 5:
                                         System.out.println("You selected quit.");
                                         System.out.println("Thank you very much for using the program.");
+                                        // Set the flag to false to break the while loop and finish the program
+                                        flag = false;
                                         break;
                                 default:
                                         System.out.println("You didnt select one of the above options.");
@@ -43,7 +50,6 @@ public class ContactBook {
                                         continue;
 
                         }
-                        break;
                 }
 
                 input.close();

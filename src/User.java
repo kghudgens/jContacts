d0fc378f6e604acec6d93e040f00b6bff72762c3
Kevin Hudgens <hudgens1073@gmail.com>
2021-09-22 19:33:17 +0900

    Created constructor for user class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,22 @@
-public class User {
+import java.util.Scanner;
 
+public class User {
+    // Make the driver object
+    // The user object will be the entity using the Driver
+    Driver driver = new Driver();
+
+    Scanner input = new Scanner(System.in);
+
+    String firstName;
+    String lastName;
+    String userName;
+
+    public User() {
+        // Welcome the user
+        System.out.println("\nWhat is your username?");
+
+        // Collect user name
+        this.userName = input.nextLine();
+        System.out.println("Hello " + userName + ", let us get started.\n");
+    }
 }

9ce43add46dbfd87c2edb817ab352122f8ef33a7
Kevin Hudgens <hudgens1073@gmail.com>
2021-09-22 19:43:24 +0900

    view function created for the user class

--- src/User.java
+++ src/User.java
@@ -19,4 +19,30 @@ public class User {
         this.userName = input.nextLine();
         System.out.println("Hello " + userName + ", let us get started.\n");
     }
+
+    public void view() {
+        System.out.println("You Selected 'VIEW'\n");
+        // Show user their options
+        System.out.println("1. All Contacts\n2. Specific Contact");
+
+        int viewAllorSpecific = input.nextInt();
+        // Consume next line because nextInt() does not
+        input.nextLine();
+
+        switch (viewAllorSpecific) {
+            case 1:
+                driver.viewAll();
+                break;
+            case 2:
+                // Collect information from user, to complete the query to database
+                System.out.println("Please enter the last name of the contact you want to see.");
+                this.lastName = input.nextLine();
+
+                // Call function that can shows specific users
+                driver.viewSpecific(this.lastName);
+                break;
+            default:
+                System.out.println("Incorrect value entered");
+        }
+    }
 }
